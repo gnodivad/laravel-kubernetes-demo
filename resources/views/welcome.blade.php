ed0ad75e1a58210e36e81f8036dcc6d34795ea28
DavidODW <dodwei.dev@gmail.com>
2018-05-15 16:13:55 +0800

    Display ip address of server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel ({{ request()->server('SERVER_ADDR') }})
                 </div>
 
                 <div class="links">
